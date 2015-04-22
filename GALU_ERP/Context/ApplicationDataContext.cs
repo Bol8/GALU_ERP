9c5ead10ff3b76548420a6c714b7c941a7f05e80
Bol8 <Oscarbolon@hotmail.com>
2015-04-22 22:04:36 +0200

    Creado modelo y mapeadas las clases

---
+++ GALU_ERP/Context/ApplicationDataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using GALU_ERP.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace GALU_ERP.Context
+{
+    public class ApplicationDataContext : IdentityDbContext<AppUser>
+    {
+        public ApplicationDataContext()
+            : base("DefaultConnection")
+        { }
+
+        public System.Data.Entity.DbSet<AppUser> AppUsers { get; set; }
+    }
+}
