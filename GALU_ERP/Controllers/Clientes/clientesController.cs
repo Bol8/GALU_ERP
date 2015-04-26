c577063561c6054e585d481e64ac26e6153b86ba
Bol8 <Oscarbolon@hotmail.com>
2015-04-26 22:31:41 +0200

    Creadas las vistas(Clientes,Proveedores,Contactos)

---
+++ GALU_ERP/Controllers/Clientes/clientesController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using GALU_ERP.Entidades;
+
+namespace GALU_ERP.Controllers.Clientes
+{
+    public class clientesController : Controller
+    {
+        private GaluEntities db = new GaluEntities();
+
+        // GET: clientes
+        public async Task<ActionResult> Index()
+        {
+            var clientes = db.clientes.Include(c => c.estado1);
+            return View(await clientes.ToListAsync());
+        }
+
+        // GET: clientes/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            cliente cliente = await db.clientes.FindAsync(id);
+            if (cliente == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cliente);
+        }
+
+        // GET: clientes/Create
+        public ActionResult Create()
+        {
+            ViewBag.Estado = new SelectList(db.estados, "idESTADOS", "Nombre");
+            return View();
+        }
+
+        // POST: clientes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "idCli,Razon_Social,NIF,NIF_R,Domicilio,CP,Poblacion,Provincia,Pais,Fecha_A,Tipo,Estado,Telefono,Mail,Web,Imagen")] cliente cliente)
+        {
+            if (ModelState.IsValid)
+            {
+                db.clientes.Add(cliente);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Estado = new SelectList(db.estados, "idESTADOS", "Nombre", cliente.Estado);
+            return View(cliente);
+        }
+
+        // GET: clientes/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            cliente cliente = await db.clientes.FindAsync(id);
+            if (cliente == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Estado = new SelectList(db.estados, "idESTADOS", "Nombre", cliente.Estado);
+            return View(cliente);
+        }
+
+        // POST: clientes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "idCli,Razon_Social,NIF,NIF_R,Domicilio,CP,Poblacion,Provincia,Pais,Fecha_A,Tipo,Estado,Telefono,Mail,Web,Imagen")] cliente cliente)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(cliente).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Estado = new SelectList(db.estados, "idESTADOS", "Nombre", cliente.Estado);
+            return View(cliente);
+        }
+
+        // GET: clientes/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            cliente cliente = await db.clientes.FindAsync(id);
+            if (cliente == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cliente);
+        }
+
+        // POST: clientes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            cliente cliente = await db.clientes.FindAsync(id);
+            db.clientes.Remove(cliente);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ GALU_ERP/Controllers/Contactos/contactosController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using GALU_ERP.Entidades;
+
+namespace GALU_ERP.Controllers.Contactos
+{
+    public class contactosController : Controller
+    {
+        private GaluEntities db = new GaluEntities();
+
+        // GET: contactos
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.contactoes.ToListAsync());
+        }
+
+        // GET: contactos/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            contacto contacto = await db.contactoes.FindAsync(id);
+            if (contacto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contacto);
+        }
+
+        // GET: contactos/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: contactos/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "idCONTACTO,Nombre,Origen,Producto")] contacto contacto)
+        {
+            if (ModelState.IsValid)
+            {
+                db.contactoes.Add(contacto);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(contacto);
+        }
+
+        // GET: contactos/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            contacto contacto = await db.contactoes.FindAsync(id);
+            if (contacto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contacto);
+        }
+
+        // POST: contactos/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "idCONTACTO,Nombre,Origen,Producto")] contacto contacto)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(contacto).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(contacto);
+        }
+
+        // GET: contactos/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            contacto contacto = await db.contactoes.FindAsync(id);
+            if (contacto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contacto);
+        }
+
+        // POST: contactos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            contacto contacto = await db.contactoes.FindAsync(id);
+            db.contactoes.Remove(contacto);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
